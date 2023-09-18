5b8f9b6dd30a2207c36302fac20819e8c2dab2d0
algvana <algv.ana@gmail.com>
2023-09-18 11:22:43 +0200

    cats

--- js/main.js
+++ js/main.js
@@ -13,22 +13,27 @@ const buttonSearch = document.querySelector('.js-button-search');
 
 const inputRace = document.querySelector('js-input-race');
 
+//ejemplo de un objeto con la información del primer gatito
+const kittenData_1 = {
+  image: 'https://dev.adalab.es/gato-siames.webp',
+  name: 'Anastacio',
+  desc: ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: 'Siamés',
+};
+const kittenData_2 = {
+  image: 'https://dev.adalab.es/sphynx-gato.webp',
+  name: 'Fiona',
+  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+  race: 'Sphynx',
+};
+const kittenData_3 = {
+  image: 'https://dev.adalab.es/maine-coon-cat.webp',
+  name: 'Cielo',
+  desc: ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad.Sus ojos son grandes y las orejas resultan largas y en punta.',
+  race: 'Maine Coon',
+};
 
-const kittenImage = 'https://dev.adalab.es/gato-siames.webp';
-const kittenName = 'Anastacio';
-const kittenDesc =
-  ' Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.';
-const kittenRace = 'Siamés';
-const kittenImage1 = 'https://dev.adalab.es/sphynx-gato.webp';
-const kittenName1 = 'Fiona';
-const kittenDesc1 =
-  'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.';
-const kittenRace1 = 'Sphynx';
-const kittenImage2 = 'https://dev.adalab.es/maine-coon-cat.webp';
-const kittenName2 = 'Cielo';
-const kittenDesc2 =
-  '   Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad.Sus ojos son grandes y las orejas resultan largas y en punta.';
-const kittenRace2 = 'Maine Coon';
+
 
 const kittenOne = `<li class="card"><article><img
     class="card_img"
@@ -93,13 +98,18 @@ function handleClickNewCatForm(event) {
   }
 }
 
-function renderKitten(url, desc, name, race) {
-  const valuePhoto = inputPhoto.value;
+function renderKitten(kittenData) {
+  /*const valuePhoto = inputPhoto.value;
   const valueDesc = inputDesc.value;
   const valueName = inputName.value;
-  const valueRace = inputRace.value;
-
+  const valueRace = inputRace.value;*/
+  this.kittenData.name = name;
+  this.KittenData.image = image;
+  this.KittenData.desc = desc;
+  this.KittenData.race = race;
+  return 
 }
+
 
 const filterKitten = (event) => {
  
